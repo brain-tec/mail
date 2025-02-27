ea2eb080645f7c169acb4b941076d86b8f1456ac
youel <younes.elahrach@smile-maroc.com>
2025-02-27 15:53:32 +0000

    [IMP] mail_template_substitute: pre-commit auto fixes

--- mail_template_substitute/__manifest__.py
+++ mail_template_substitute/__manifest__.py
@@ -6,10 +6,10 @@
     "summary": """
         This module allows to create substitution rules for mail templates.
         """,
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV," "Odoo Community Association (OCA)",
-    "website": "https://github.com/OCA/social",
+    "website": "https://github.com/OCA/mail",
     "depends": ["base", "mail"],
     "data": [
         "security/mail_template_substitution_rule.xml",
