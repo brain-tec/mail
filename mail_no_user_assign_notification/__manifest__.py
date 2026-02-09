1800c605f32475d493ab532397f22dfe2c7392c7
Jorge Quinteros <jquinteros@sdi.es>
2026-02-10 00:25:32 +0100

    [MIG] mail_no_user_assign_notification: Migration to 18.0

--- mail_no_user_assign_notification/__manifest__.py
+++ mail_no_user_assign_notification/__manifest__.py
@@ -4,7 +4,7 @@
     "name": "Mail No user Assign Notification",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/mail",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "depends": ["mail"],
     "license": "AGPL-3",
     "category": "Discuss",
